c44fc5b3294ecf4bb1e45c69917443f43cbfda02
Abdo <algnidy1@gmail.com>
2025-08-01 01:40:47 +0300

    save some edit

--- script.js
+++
@@ -1,8 +0,0 @@
-  const menuBtn = document.getElementById("menu-btn");
-  const menu = document.getElementById("menu");
-
-  menuBtn.addEventListener("click", () => {
-    const isExpanded = menuBtn.getAttribute("aria-expanded") === "true";
-    menuBtn.setAttribute("aria-expanded", !isExpanded);
-    menu.classList.toggle("hidden");
-  });
